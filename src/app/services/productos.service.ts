972ff38c33a37d2a22a289a4abd3c712b9b1473d
Luis Carlos Cornejo Hernández <luiscarlos.cornejo@gmail.com>
2017-11-01 11:36:00 -0600

    Add firebase to portafolio

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -3,9 +3,9 @@ import { Http } from '@angular/http';
 
 @Injectable()
 export class ProductosService {
-
+ 
   productos:any[] = [];
-  cargando:boolean = false;
+  cargando:boolean = true;
 
   constructor( private http:Http ) {
 
@@ -14,16 +14,14 @@ export class ProductosService {
 
 public cargar_productos(){
 
-  if (this.productos.length === 0){
-
     this.cargando = true;
       this.http.get('https://sitioweb-1b830.firebaseio.com/productos_idx.json')
       .subscribe (res => {
 
         console.log(res.json());
         this.cargando = false;
+        this.productos = res.json();
       });
-  }
 }
 
 }
