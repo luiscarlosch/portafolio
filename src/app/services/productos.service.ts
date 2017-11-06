a920035fca484dc91075afbc02d16d8664f4d060
Luis Carlos Cornejo Hernández <luiscarlos.cornejo@gmail.com>
2017-11-05 23:10:08 -0600

    First version

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -1,17 +1,48 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 
+
 @Injectable()
 export class ProductosService {
 
   productos:any[] = [];
+  productos_filtrado:any[] = [];
   cargando:boolean = true;
 
   constructor( private http:Http ) {
 
     this.cargar_productos();
+
   }
 
+public buscar_producto ( termino:string ){
+
+  // console.log("Buscando Producto");
+  // console.log( this.productos.length );
+
+  if (this.productos.length === 0 ){
+      this.cargar_productos().then( ()=>{
+        // termino la carga
+        this.filtrar_productos(termino);
+      });
+    }else{
+        this.filtrar_productos(termino);
+      }
+}
+
+  private filtrar_productos(termino:string){
+
+    this.productos_filtrado = [];
+    termino = termino.toLowerCase();
+
+    this.productos.forEach( prod => {
+
+      if(prod.categoria.indexOf( termino ) >=0 || prod.titulo.toLowerCase().indexOf(termino) >=0){
+        this.productos_filtrado.push( prod );
+    }
+      //  console.log (prod);
+    })
+}
 public cargar_producto( cod:string){
 
 return this.http.get(`https://sitioweb-1b830.firebaseio.com/productos/${ cod }.json`);
@@ -19,18 +50,26 @@ return this.http.get(`https://sitioweb-1b830.firebaseio.com/productos/${ cod }.j
 
 public cargar_productos(){
 
-    this.cargando = true;
+this.cargando = true;
+  let promesa = new Promise( ( resolve, reject)=>{
+
+
       this.http.get('https://sitioweb-1b830.firebaseio.com/productos_idx.json')
       .subscribe (res => {
 
         // console.log(res.json());
 
-        setTimeout( ()=>{
+        // setTimeout( ()=>{
           this.cargando = false;
           this.productos = res.json();
-        }, 1500)
+          resolve();
+        // }, 1500)
 
       });
-}
 
+  });
+
+  return promesa;
+
+}
 }
